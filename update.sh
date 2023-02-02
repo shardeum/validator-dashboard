8b0653c01a568a7e8a94199afaee24719484ac8d
Glitch18 <atharvakn@gmail.com>
2023-02-02 18:02:27 +0530

    Implement update.sh script

---
+++ update.sh
@@ -0,0 +1,27 @@
+command -v docker >/dev/null 2>&1 || { echo >&2 "'docker' is required but not installed. See https://gitlab.com/shardeum/validator/dashboard/-/tree/dashboard-gui-nextjs#how-to for details."; exit 1; }
+if command -v docker-compose &>/dev/null; then
+  echo "docker-compose is installed on this machine"
+elif docker --help | grep -q "compose"; then
+  echo "docker compose subcommand is installed on this machine"
+else
+  echo "docker-compose or docker compose is not installed on this machine"
+  exit 1
+fi
+
+docker-safe() {
+  if ! command -v docker &>/dev/null; then
+    echo "docker is not installed on this machine"
+    exit 1
+  fi
+
+  if ! docker $@; then
+    echo "Trying again with sudo..."
+    sudo docker $@
+  fi
+}
+
+echo "Updating validator image and rebuilding docker..."
+./docker-down.sh
+./cleanup.sh
+docker-safe build --no-cache -t test-dashboard -f Dockerfile --build-arg RUNDASHBOARD=y .
+./docker-up.sh
