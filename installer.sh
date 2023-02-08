ddb008de9ef5d4aac1934c66f1b812ac7ac01561
CombatPug <p.ladar@icloud.com>
2023-02-08 15:40:46 +0100

    fix: sed command for macos needs an empty string parameter
    fix: use if-else construct for different architecture

--- installer.sh
+++ installer.sh
@@ -216,10 +216,16 @@ cat <<EOF
 
 EOF
 
-cd ${NODEHOME} &&
-sed -i "s/- '8080:8080'/- '$DASHPORT:$DASHPORT'/" docker-compose.yml &&
-sed -i "s/- '9001-9010:9001-9010'/- '$SHMEXT:$SHMEXT'/" docker-compose.yml &&
-sed -i "s/- '10001-10010:10001-10010'/- '$SHMINT:$SHMINT'/" docker-compose.yml &&
+cd ${NODEHOME}
+if [[ "$(uname)" == "Darwin" ]]; then
+  sed -i '' "s/- '8080:8080'/- '$DASHPORT:$DASHPORT'/" docker-compose.yml
+  sed -i '' "s/- '9001-9010:9001-9010'/- '$SHMEXT:$SHMEXT'/" docker-compose.yml
+  sed -i '' "s/- '10001-10010:10001-10010'/- '$SHMINT:$SHMINT'/" docker-compose.yml
+else
+  sed -i "s/- '8080:8080'/- '$DASHPORT:$DASHPORT'/" docker-compose.yml
+  sed -i "s/- '9001-9010:9001-9010'/- '$SHMEXT:$SHMEXT'/" docker-compose.yml
+  sed -i "s/- '10001-10010:10001-10010'/- '$SHMINT:$SHMINT'/" docker-compose.yml
+fi
 ./docker-up.sh
 
 echo "Starting image. This could take a while..."
