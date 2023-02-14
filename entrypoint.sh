fdd75c6d22076fef2126c15d4ff6f43a8874d7eb
Glitch18 <atharvakn@gmail.com>
2023-02-14 23:14:34 +0530

    suppress deprecated warnings for npm installs

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,14 +14,14 @@ git clone https://gitlab.com/shardeum/validator/cli.git
 
 echo "Install the CLI"
 cd cli
-npm i && npm link
+npm i --silent && npm link
 cd ..
 
 git clone https://gitlab.com/shardeum/validator/gui.git
 
 echo "Install the GUI"
 cd gui
-npm i
+npm i --silent
 npm run build
 #openssl req -x509 -nodes -days 99999 -newkey rsa:2048 -keyout ./selfsigned.key -out selfsigned.crt -subj "/C=US/ST=Texas/L=Dallas/O=Shardeum/OU=Shardeum/CN=shardeum.org"
 
