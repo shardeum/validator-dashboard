fe792de0956a4ff7c02ec007e9a22d1e55b7190e
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-06-13 16:44:24 +0530

    reverted change

--- installer.sh
+++ installer.sh
@@ -526,7 +526,13 @@ fi
 
 git clone https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }
 cd ${NODEHOME}
+sed -i '' -e '/^cd gui$/{
+r /dev/stdin
+d
+}' entrypoint.sh <<'EOF'
+cd gui
 git checkout new-design
+EOF
 chmod a+x ./*.sh
 
 cat <<EOF
