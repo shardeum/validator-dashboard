5b06f985219cbd970ce72dc9c18c306d3f0a3bf8
Chris Freels <cafreels@gmail.com>
2024-06-25 16:16:33 -0500

    new gui old beta

--- installer.sh
+++ installer.sh
@@ -524,7 +524,7 @@ if [ -d "$NODEHOME" ]; then
   fi
 fi
 
-git clone https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }
+git clone -b qa-103-new-gui https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }
 cd ${NODEHOME}
 chmod a+x ./*.sh
 
