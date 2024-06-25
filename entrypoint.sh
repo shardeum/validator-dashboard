62e0e8395b2874cc61ccb3592c23435ef3b66ee7
Chris Freels <cafreels@gmail.com>
2024-06-25 16:35:08 -0500

    Update validator-gui to new-design branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,7 @@ cd cli
 npm i --silent && npm link
 cd ..
 
-git clone https://github.com/shardeum/validator-gui.git gui
+git clone -b new-design https://github.com/shardeum/validator-gui.git gui
 
 echo "Install the GUI"
 cd gui
