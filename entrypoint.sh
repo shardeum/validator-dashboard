875e6adaf7e4166549dca5fca68218d61ac23b10
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-07-18 18:35:39 +0530

    corrected clone command

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,14 +24,14 @@ echo "/home/node/.pm2/logs/*.log /home/node/app/cli/build/logs/*.log {
 
 # Pull latest versions of the CLI and GUI
 
-git -b new-design clone https://github.com/shardeum/validator-cli.git cli
+git clone -b new-design https://github.com/shardeum/validator-cli.git cli
 
 echo "Install the CLI"
 cd cli
 npm i --silent && npm link
 cd ..
 
-git -b new-design clone https://github.com/shardeum/validator-gui.git gui
+git clone -b new-design https://github.com/shardeum/validator-gui.git gui
 
 echo "Install the GUI"
 cd gui
