eae22bb29a6607ab0d0340a42413b8204173d864
Adrian Rojas <arojasgomez.93@gmail.com>
2024-08-20 12:26:26 -0300

    Modify installer.sh git clone to -b beta1.12.2

--- installer.sh
+++ installer.sh
@@ -524,8 +524,7 @@ if [ -d "$NODEHOME" ]; then
   fi
 fi
 
-git clone https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }
-cd ${NODEHOME}
+git clone -b beta1.12.2 https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }cd ${NODEHOME}
 chmod a+x ./*.sh
 
 cat <<EOF
