0c64fbb2439888f9a1728b8a5522f50784de8b05
chrypnotoad <chris@shardeum.org>
2024-11-13 14:38:44 -0600

    fix branch name

--- installer.sh
+++ installer.sh
@@ -585,7 +585,7 @@ if [ -d "$NODEHOME" ]; then
   fi
 fi
 
-git clone -b itn1.5.2 https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }
+git clone -b itn1.15.2 https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }
 cd ${NODEHOME}
 chmod a+x ./*.sh
 
