ca047b1c3d7792a2dc4f8f4d06b8b99aea5f1e8a
Atharva <atharvakn@gmail.com>
2024-11-18 14:59:02 +0530

    feat: explicit clone main branch

--- installer.sh
+++ installer.sh
@@ -585,7 +585,7 @@ if [ -d "$NODEHOME" ]; then
   fi
 fi
 
-git clone https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }
+git clone -b main https://github.com/shardeum/validator-dashboard.git ${NODEHOME} || { echo "Error: Permission denied. Exiting script."; exit 1; }
 cd ${NODEHOME}
 chmod a+x ./*.sh
 
